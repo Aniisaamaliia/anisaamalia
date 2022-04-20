18e833dfc88c41d15fb75c60dae7b7fb635d086e
Anisa Amalia <aniisaamaliia@gmail.com>
2022-04-20 20:18:03 +0800

    profilwebsite

---
+++ script.js
@@ -0,0 +1,131 @@
+
+
+//------landing-HOME-TYPEWRITER EFFECT
+
+
+let line = document.getElementById("slogan")
+let txts = ['a student', 'a coder', 'also a hard worker'];
+let speed = 50
+
+async function typewriter(txt) {
+    for (let i = 0; i < txt.length; i++) {
+        line.innerHTML += txt[i];
+        await delay(speed)
+    }
+}
+
+async function reverseTypewriter(txt) {
+    for (let i = txt.length; i > 0; i--) {
+        line.innerHTML = line.innerHTML.slice(0, -1)
+        await delay(speed)
+    }
+}
+
+async function writeLoop() {
+    for (let i = 0; i < txts.length; i++) {
+        await typewriter(txts[i])
+        await delay(1000)
+        await reverseTypewriter(txts[i])
+        await delay(300)
+    }
+    writeLoop()
+}
+
+function delay(ms) {
+    return new Promise((resolve) => { setTimeout(() => { resolve() }, ms) })
+}
+
+writeLoop()
+
+let modalId = $('#image-gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
+    }
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
+
+// build key actions
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
